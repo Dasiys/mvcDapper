baf0773177d97875219d214b1f4aca844108a6d0
Dasiys <yeweimi@iCloud.com>
2017-05-24 16:07:19 +0800

    增加了Expression的Demo

---
+++ Expression/Expression/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dtoUser = GetExpression()(new User() {Name = "叶伟密"});
+            Console.WriteLine(dtoUser.Name);
+            Console.ReadLine();
+        }
+
+        static Func<User, UserDto> GetExpression()
+        {
+            ParameterExpression param = System.Linq.Expressions.Expression.Parameter(typeof(User), "c");
+            MemberExpression mem = System.Linq.Expressions.Expression.Property(param, "Name");
+
+            var loc = System.Linq.Expressions.Expression.Variable(typeof(UserDto), "dto");
+            NewExpression newExpression = System.Linq.Expressions.Expression.New(typeof(UserDto));
+            BinaryExpression newDto = System.Linq.Expressions.Expression.Assign(loc, newExpression);
+
+            MemberExpression meml = System.Linq.Expressions.Expression.Property(loc, "Name");
+            BinaryExpression asn = System.Linq.Expressions.Expression.Assign(meml, mem);
+
+            LabelTarget labelTarget = System.Linq.Expressions.Expression.Label(typeof(UserDto));
+            GotoExpression gt = System.Linq.Expressions.Expression.Return(labelTarget, loc);
+            LabelExpression lb = System.Linq.Expressions.Expression.Label(labelTarget,System.Linq.Expressions.Expression.Constant(new UserDto() { Name = "" }));
+
+            BlockExpression blocks = System.Linq.Expressions.Expression.Block(new[] { loc }, loc,
+                newDto, asn, gt,lb);
+            Expression<Func<User, UserDto>> dt = System.Linq.Expressions.Expression.Lambda<Func<User, UserDto>>(blocks, param);
+            return dt.Compile();
+        }
+    }
+}
---
+++ Expression/Expression/UserDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expression
+{
+    public class UserDto
+    {
+        /// <summary>
+        /// 设置或获取名称
+        /// </summary>
+        public string Name { set; get; }
+        /// <summary>
+        /// 设置或获取尊称
+        /// </summary>
+        public string RespectName => $"尊称:{Name}";
+    }
+}
